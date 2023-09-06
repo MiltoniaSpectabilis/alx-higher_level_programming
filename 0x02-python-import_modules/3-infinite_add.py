3b4a192e514cdd89b71f1f547fa325d3f9b04277
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-06 04:56:12 +0100

    fix indent

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-total = 0
-for i in range(1, len(sys.argv)):
-    total += int(sys.argv[i])
+    total = 0
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
     print("{}".format(total))
