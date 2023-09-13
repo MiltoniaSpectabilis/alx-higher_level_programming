e4846edf7f1b921b96754653a49336f367d568bd
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-13 07:10:30 +0100

    remove

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ import hidden_4
 
 if __name__ == "__main__":
     names = dir(hidden_4)
-    for char in names:
-        if char[:2] != "__":
-            print(char)
+        for char in names:
+            if char[:2] != "__":
+                print(char)
