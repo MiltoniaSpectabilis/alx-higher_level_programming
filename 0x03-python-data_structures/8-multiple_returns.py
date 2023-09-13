dc6df7ade40b4f2ae9fd8aebfda552d4986416c0
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-13 03:19:40 +0100

    trial

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-
 def multiple_returns(sentence):
     if sentence == "":
-        return (0, None)
+        return None
     else:
         return (len(sentence), sentence[0])
