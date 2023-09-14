aa7c5143775c64ab84202e27794b491f40ccf559
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-14 03:46:26 +0100

    alt

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-    for key in sorted_keys:
-        print(key, a_dictionary[key])
+    for k in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary[k]))
