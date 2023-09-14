ad889c22533123c66af2274d0e8569ebf6e5f6c7
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-14 03:41:15 +0100

    alt

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,8 +2,4 @@
 
 
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    list_keys = list(new_dict.keys())
-    for i in list_keys:
-        new_dict[i] *= 2
-    return (new_dict)
+    return {k: v * 2 for k, v in a_dictionary.items()}
