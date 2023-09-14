8aaf29d11d4371d0a61afbc4d20accf5a3c80e61
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-14 03:39:04 +0100

    fix arg

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def multiply_by_2(a_dictionary):
+def multiply_by_2(mydictionary):
     new_dict = mydictionary.copy()
     list_keys = list(new_dict.keys())
     for i in list_keys:
