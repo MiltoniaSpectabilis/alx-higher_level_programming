dd5bf687295251e8a272b44140540cc3687b3991
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-27 01:02:03 +0100

    add class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): The size of the square.
+        """
+        self.__size = size
