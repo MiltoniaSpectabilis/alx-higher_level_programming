08c915f6e4ecb066df0123181efadfb93805a9b3
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-06-19 06:26:19 +0100

    ss

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
 } else {
-  console.log('Arguments found');
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
