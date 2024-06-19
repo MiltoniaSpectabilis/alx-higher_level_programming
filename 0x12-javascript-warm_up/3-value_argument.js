15e8832f3d1f8d72fac394a26d176d8ce8b2f255
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-06-19 04:38:51 +0100

    ss

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,13 +4,13 @@ const args = process.argv.slice(2);
 
 if (!args[0])
 {
-  console.log("No argument");
+  console.log('No argument');
 }
 else if (!args[1])
 {
-  console.log("Argument found");
+  console.log('Argument found');
 }
 else
 {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
