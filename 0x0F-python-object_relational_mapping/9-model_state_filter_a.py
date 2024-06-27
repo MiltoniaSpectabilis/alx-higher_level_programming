d0a9e6fd156ba8c0ecd8954d5c69c91dbe458f02
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-06-27 18:05:37 +0100

    pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all State objects containing the letter 'a' from the database hbtn_0e_6_usa
+Lists all State objects containing the letter 'a'
+from the database hbtn_0e_6_usa
 """
 
 import sys
@@ -19,7 +20,8 @@ if __name__ == "__main__":
     session = Session()
 
     # Query State objects containing 'a', order by states.id
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%'))
+    .order_by(State.id).all()
 
     # Display the results
     for state in states:
