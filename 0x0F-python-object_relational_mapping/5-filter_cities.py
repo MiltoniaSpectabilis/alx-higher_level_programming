8a94857d0752a183ca1ad18ead30852cd3ef7a64
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-06-27 17:44:29 +0100

    all cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM cities
+                   JOIN states ON cities.state_id = states.id
+                   WHERE states.name = %s
+                   ORDER BY cities.id""", (argv[4],))
+    print(", ".join([row[0] for row in cur.fetchall()]))
+    cur.close()
+    db.close()
