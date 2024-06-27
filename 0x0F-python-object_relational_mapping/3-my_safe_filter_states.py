7fbcde5e5850f07ebb7767e574a6ff2fc5586464
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-06-27 14:34:37 +0100

    injection and other fixes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,18 +5,18 @@ in the states table of hbtn_0e_0_usa
 where name matches the argument
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                 WHERE name LIKE BINARY '{}'\
-                 ORDER BY states.id ASC".format(sys.argv[4]))
+                 WHERE states.name = %s\
+                 ORDER BY states.id ASC", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
