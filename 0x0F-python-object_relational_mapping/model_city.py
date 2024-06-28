59efc3c173638df5152522b81464370640ed1967
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-06-28 23:23:27 +0100

    add docstring

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,6 +7,9 @@ from model_state import Base
 
 
 class City(Base):
+    """
+    City class that inherits from Base
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
