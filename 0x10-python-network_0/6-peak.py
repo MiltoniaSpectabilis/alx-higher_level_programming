113edee1366a9e2187a0477515b1f0fc11b41403
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-07-05 01:25:31 +0100

    doc

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def find_peak(list_of_integers):
+"""Return a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
     left, right = 0, len(list_of_integers) - 1
