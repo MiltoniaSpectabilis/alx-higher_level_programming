f27a9f1ce1e586db8996461c056e050b2ecd133c
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-07-31 02:07:09 +0100

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,9 +5,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (err, res) => {
-	if (err) console.log(err);
-	else {
-		console.log(`code: ${res.statusCode}`);
-	}
+  if (err) console.log(err);
+  else {
+    console.log(`code: ${res.statusCode}`);
+  }
 });
-
