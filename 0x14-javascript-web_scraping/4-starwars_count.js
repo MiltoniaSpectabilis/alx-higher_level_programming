5f44bbd8037572a1a876f60223e802e3029e6af3
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-07-31 02:48:47 +0100

    semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,9 +3,8 @@
 const request = require('request');
 
 request.get(process.argv[2], (err, res, body) => {
-	if (err) return console.error(err);
+  if (err) return console.error(err);
 
-	const moveId = body.split('people/18/').length - 1;
-	console.log(moveId);
+  const moveId = body.split('people/18/').length - 1;
+  console.log(moveId);
 });
-
