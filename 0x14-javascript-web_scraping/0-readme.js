1474a7f7468faf7d26b929f4a424d265096b34f8
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-07-31 01:38:12 +0100

    modify code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) throw err;
-	console.log(data);
+  if (err) console.log(err);
+  console.log(data);
 });
-
