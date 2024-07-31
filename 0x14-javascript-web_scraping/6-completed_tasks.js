7f0533cc3cfeb04e9357b355dfc1e39547e1983d
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-07-31 03:04:11 +0100

    semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,14 +3,14 @@
 const request = require('request');
 
 request.get(process.argv[2], (err, res, body) => {
-	const todos = JSON.parse(body);
-	const result = todos.reduce((acc, todo) => {
-		if (todo.completed) {
-			acc[todo.userId] = (acc[todo.userId] || 0) + 1;
-		}
-		return acc;
-	}, {});
+  if (err) return console.error(err);
+  const todos = JSON.parse(body);
+  const result = todos.reduce((acc, todo) => {
+    if (todo.completed) {
+      acc[todo.userId] = (acc[todo.userId] || 0) + 1;
+    }
+    return acc;
+  }, {});
 
-	console.log(result);
+  console.log(result);
 });
-
