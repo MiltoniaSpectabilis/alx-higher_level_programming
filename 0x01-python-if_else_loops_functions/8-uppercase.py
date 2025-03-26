1a4c19ae969b316fb8ceecdfeb3fd6b4f17b1af3
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-03-26 05:38:36 +0000

    use ternary operator to reduce print() num to 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if "a" <= c <= "z":
-            print("{}".format(chr(ord(c) - 32)), end='')
+        print("{:c}".format(
+            ord(c) - 32
+            if 97 <= ord(c) <= 122
+            else ord(c)), end=''
+        )
     print()
