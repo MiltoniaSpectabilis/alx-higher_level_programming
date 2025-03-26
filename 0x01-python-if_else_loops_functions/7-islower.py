bb5980e5f91c42d8d3788b24029d3eab5c2aa52f
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-03-26 04:22:30 +0000

    remove hardcoded unicodes

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
-    # char = ord(c)
-    if 'a' <= c <= 'z':
+    char_code = ord(c)
+    a_code = ord('a')
+    z_code = ord('z')
+
+    if a_code <= char_code <= z_code:
         return True
     else:
         return False
