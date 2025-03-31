02cebfa65a05c1252e9161f7a9434f43f8a24931
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-03-31 20:34:26 +0000

    return the result instead of printing it

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,5 @@ def square_matrix_simple(matrix=[]):
     result = []
     for row in matrix:
         squared_row = map(lambda x: x ** 2, row)
-        print(squared_row)
         result.append(list(squared_row))
-    print(result)
+    return result
