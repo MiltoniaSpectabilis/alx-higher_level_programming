7f2a13862c23832876188f7c3c1a1dae92381628
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-01 01:22:33 +0000

    remove func call

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,7 +3,3 @@
 def print_sorted_dictionary(a_dictionary):
     for key, value in sorted(a_dictionary.items()):
         print(f"{key}: {value}")
-
-
-print_sorted_dictionary(
-    {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]})
