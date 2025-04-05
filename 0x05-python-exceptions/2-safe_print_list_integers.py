b4a1e738e9424fb8b0bd03ff713da250b157a17f
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-05 04:24:50 +0000

    a func that prints the first x elements of a list and only ints

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for element in my_list[:x]:
+            if isinstance(element, int):
+                print("{:d}".format(element), end='')
+                count += 1
+            else:
+                continue
+        print()
+        return count
+    except (ValueError, TypeError):
+        print()
+        return count
