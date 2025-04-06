34237c0d5ac0040b486f05e2ef2d5c7f8d901c42
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-06 04:01:21 +0100

    add finally block

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,16 +3,18 @@
 def list_division(my_list_1, my_list_2, list_length):
     res_div = []
     i = 0
-    for i in range(list_length):
-        try:
-            res_div.append(my_list_1[i] / my_list_2[i])
-        except TypeError:
-            print("wrong type")
-            res_div.append(0)
-        except ZeroDivisionError:
-            print('division by 0')
-            res_div.append(0)
-        except IndexError:
-            print("out of range")
-            res_div.append(0)
-    return res_div
+    try:
+        for i in range(list_length):
+            try:
+                res_div.append(my_list_1[i] / my_list_2[i])
+            except TypeError:
+                print("wrong type")
+                res_div.append(0)
+            except ZeroDivisionError:
+                print('division by 0')
+                res_div.append(0)
+            except IndexError:
+                print("out of range")
+                res_div.append(0)
+    finally:
+        return res_div
