91cc10e5922dac875d4dcbe77b22383770ea51af
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-06 03:55:07 +0100

    division between elements of two lists and catching errors with try/except

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    res_div = []
+    i = 0
+    for i in range(list_length):
+        try:
+            res_div.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            res_div.append(0)
+        except ZeroDivisionError:
+            print('division by 0')
+            res_div.append(0)
+        except IndexError:
+            print("out of range")
+            res_div.append(0)
+    return res_div
