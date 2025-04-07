92a6e36d2f40d61fc599d200acde87a8cb2aadad
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-07 23:27:17 +0100

    document program following pydoc conventions

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""a module that defines a Square class"""
+
 
 class Square:
+    """a class representing a square"""
+
     def __init__(self):
+        """initialize a Square instance"""
         pass
