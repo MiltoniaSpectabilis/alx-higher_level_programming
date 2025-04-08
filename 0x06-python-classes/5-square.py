6d7d8a05e1424888fc1e4efd669b8048134d7e33
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-08 06:09:33 +0100

    a class Square that defines a square based on 4-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""a module that defines a Square class"""
+
+
+class Square:
+    """a class representing a square"""
+
+    def __init__(self, size=0):
+        """initialize a Square instance"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for row in range(self.__size):
+            for item in range(self.__size):
+                print("#", end='')
+            print()
+
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
