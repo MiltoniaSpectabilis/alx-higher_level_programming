9d45966beaf412e865d820ef7642c14082d2f117
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-08 04:43:13 +0100

    a class Square that defines a square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a module that defines a Square class"""
+
+
+class Square:
+    """a class representing a square"""
+
+    def __init__(self, size):
+        """initialize a Square instance"""
+        self.__size = size
