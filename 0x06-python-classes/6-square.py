704caf2054a3968b499f03147fca3e9eecd2b7ed
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-09 01:03:52 +0100

    another one

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,7 +28,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, int) and not all(val >= 0 for val in value):
+        if (
+            not isinstance(value, tuple) or len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -41,8 +45,5 @@ class Square:
         else:
             for _ in range(self.__position[1]):
                 print()
-            for row in range(self.__size):
-                print(f"{' ' * self.__position[0]}", end='')
-                for item in range(self.__size):
-                    print("#", end='')
-                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
