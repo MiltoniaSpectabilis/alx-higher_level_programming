78c7c0e479730b08c956d0050b736cc361835558
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-11 20:51:12 +0100

    edit code so when one string is passed a trailing space is kept

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,9 +10,9 @@ def say_my_name(first_name, last_name=""):
     A function that prints
     'My name is <first_name> <last_name>'
     """
-    if last_name == "" and isinstance(first_name, str):
-        print(f"My name is {first_name}")
-    elif not isinstance(first_name, str):
+    # if not last_name and isinstance(first_name, str):
+    #     print(f"My name is {first_name}")
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
