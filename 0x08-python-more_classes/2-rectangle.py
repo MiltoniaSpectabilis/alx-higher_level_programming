aa3d62ab5e898932973ae883c9b6cce7c88aa1b7
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-15 00:13:03 +0100

    implement public instance methods perimeter and area

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""a module that defines a class Rectangle"""
+
+
+class Rectangle:
+    """a class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """attribute initialization"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
+
+
+my_rec = Rectangle(2, 4)
+print(f"Area: {my_rec.area()} - Perimeter: {my_rec.perimeter()}")
+print("--")
+my_rec.width = 10
+my_rec.height = 3
+print(f"Area: {my_rec.area()} - Perimeter: {my_rec.perimeter()}")
