e8dae08ecdbbb0ac47583e80addecc8e5bfaf2a4
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-17 09:25:39 +0100

    implement a public instance method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
