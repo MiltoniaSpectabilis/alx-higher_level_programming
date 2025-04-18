c4ff8c510cd9cf1569dbd98cdd483d33fe1fd84c
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-18 13:33:46 +0100

    remove test calls and add docstrings

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines a function writes a specified text
+to a specified file
+"""
 
 
 def write_file(filename="", text=""):
+    """
+    writes a specified text to a specified file
+    and returns the number of written chars
+    """
     with open(filename, "w") as f:
         f.write(text)
 
     return len(text)
-
-
-print(write_file("file.txt", "Henlo worlnd"))
