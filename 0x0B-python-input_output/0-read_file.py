36ff75147d6145be36b94db0acf980310586386c
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-18 13:28:53 +0100

    fix and simplify code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ reads and prints the content of a file
 
 def read_file(filename=""):
     """reads and prints the content of the file"""
-    with open(f"{filename}", "r") as file:
-        file_content = file.read()
-        print(file_content, end='')  # remove new line at the end of content
+    with open(filename, "r") as f:
+        content = f.read()
+        print(content, end='')  # remove new line at the end of content
