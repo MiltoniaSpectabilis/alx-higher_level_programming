78539531973cd73266a937bc7c677e58e530e662
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-18 12:50:48 +0100

    prevent code from adding new line to the end of the file content

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     """reads and prints the content of the file"""
     with open(f"{filename}", "r") as file:
         file_content = file.read()
-        print(file_content)
+        print(file_content, end='')  # remove new line at the end of content
