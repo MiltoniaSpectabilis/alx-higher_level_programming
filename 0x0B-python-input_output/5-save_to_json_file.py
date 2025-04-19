71371a622ae7b3f4aa90247af5a95931cbe9582d
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-19 08:28:41 +0100

    change the order of the code so the file is opened first and then the serialization happens, not the opposite

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,6 +12,6 @@ def save_to_json_file(my_obj, filename):
     This function serializes a specified object and then writes
     it into a specified file
     """
-    json_str = json.dumps(my_obj)
     with open(filename, "w") as f:
+        json_str = json.dumps(my_obj)
         f.write(json_str)
