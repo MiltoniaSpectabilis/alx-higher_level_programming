ce13ee09722c3d45f32c63e849dd737e5abab212
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-21 23:44:53 +0100

    Implement a static method that returns a deserialized list of a json string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,3 +51,9 @@ class Base:
                 list_dicts.append(obj.to_dictionary())
             with open(f"{cls.__name__}.json", "w") as f:
                 f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
