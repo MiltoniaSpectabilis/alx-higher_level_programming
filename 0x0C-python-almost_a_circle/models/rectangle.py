82e7765018e5c96bb3855cc75d35e7b811cf829c
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-21 10:23:04 +0100

    implement validation logic for all setters of Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """Sets the width of the rectangle."""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -40,6 +44,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """Sets the height of the rectangle."""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -50,6 +58,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """Sets the x-coordinate of the rectangle."""
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -60,4 +72,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """Sets the y-coordinate of the rectangle."""
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
