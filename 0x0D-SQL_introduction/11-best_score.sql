278c7e6fc72ebbbc606c6eead4cba4fcde2d1f33
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-01 10:29:21 +0100

    Implement a simple sql script that lists all rows with a score >= 10 in second_table

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,8 @@
+-- Select data (score and name)
+SELECT score, name
+-- From second_table
+FROM second_table
+-- Where the score is greater than or equal to 10
+WHERE (score >= 10)
+-- Order data by descending score
+ORDER BY score DESC;
