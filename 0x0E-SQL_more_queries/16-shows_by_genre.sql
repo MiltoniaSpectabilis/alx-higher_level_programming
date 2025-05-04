3d4df34a5b2339b6163cec112f6703f3affe43d0
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-04 19:23:13 +0100

    Fix code

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,7 +6,7 @@ FROM
     tv_shows
 LEFT JOIN
     tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-JOIN
+LEFT JOIN
     tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY
     tv_shows.title ASC,
