c8824e3f615e753f63f8e127c1599ff81fe13dd5
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-04 15:37:48 +0100

    Change queries to lowercase

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 --
-select tv_shows.title, tv_show_genres.genre_id
-from tv_shows
-left join tv_show_genres on tv_shows.id = tv_show_genres.show_id
-order by tv_shows.title asc,
-tv_show_genres.genre_id asc;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC,
+tv_show_genres.genre_id ASC;
