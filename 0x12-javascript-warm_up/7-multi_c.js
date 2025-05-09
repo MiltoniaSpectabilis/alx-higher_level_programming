0d8a467b16b872c1178e4e83aa78c03d99989edd
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-09 16:05:57 +0100

    Simplify the code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
 const strToPrint = 'C is fun';
+const numberOfIterations = process.argv[2];
 
-// check if the arg is provided
-if (!process.argv[2]) {
+// check if the arg can be converted to an int
+// it implicitly converts the arg to an int if true
+if (!Number(numberOfIterations)) {
   console.log('Missing number of occurrences');
 }
-// convert the str to an int
-const numberOfIterations = Number(process.argv[2]);
 
-// print the str according to the arg
+// print the str according to the arg.
 for (let i = 0; i < numberOfIterations; i++) {
   console.log(strToPrint);
 }
