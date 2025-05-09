f29db09ba697246fb80c984341568d1421ca88f3
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-09 16:18:35 +0100

    Implement a script that prints a square based on the arg/num passed to it

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const numOfIters = process.argv[2];
+
+if (!Number(numOfIters)) {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < numOfIters; i++) {
+  for (let j = 0; j < numOfIters; j++) {
+    process.stdout.write('X');
+  }
+  process.stdout.write('\n');
+}
