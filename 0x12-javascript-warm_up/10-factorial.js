fcc7226a7a46ad478e78edb5634a025309f524f6
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-09 17:03:06 +0100

    Implement a js script that prints a factorial with recursion using a number passed to it as an arg

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+function factorial(n) {
+  if (n <= 1 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const arg = process.argv[2];
+const n = Number(arg);
+console.log(factorial(n));
