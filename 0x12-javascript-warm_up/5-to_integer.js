21ad8f9cd304cfef34d05dee3881e24b32dcdad5
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-09 15:02:18 +0100

    Implement a js script that prints the first int passed to it as an arg in a specified format

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+//
+const argStr = process.argv[2];
+const argNum = Number(argStr);
+
+if (Number.isInteger(argNum)) {
+  console.log(`My number: ${argNum}`);
+} else {
+  console.log('Not a number');
+}
