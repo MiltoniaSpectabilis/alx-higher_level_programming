4e6a4a952deefa7e6a68ad6f5f7ed114b3ec3685
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-09 15:39:26 +0100

    Implement a js script that prints a specified str an 'x' num of times depending on the arg passed to it

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const strToPrint = 'C is fun';
+
+// check if the arg is provided
+if (!process.argv[2]) {
+  console.log('Missing number of occurrences');
+}
+// convert the str to an int
+const numberOfIterations = Number(process.argv[2]);
+
+// print the str according to the arg
+for (let i = 0; i < numberOfIterations; i++) {
+  console.log(strToPrint);
+}
