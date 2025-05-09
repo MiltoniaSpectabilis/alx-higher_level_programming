2a6ae10f74409a00af97c1e3951186267c2dc783
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-09 15:15:37 +0100

    Implement a js script that print 3 specified lines using a (for) loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+//
+const langArr = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < langArr.length; i++) {
+  console.log(langArr[i]);
+}
