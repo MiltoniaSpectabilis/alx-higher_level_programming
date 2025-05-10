1e187d12d04531f2a0925f551e6d40cd9c5c31c9
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-10 15:56:08 +0100

    Implement a Square class that inherits from Square base class that prints a square using a specified char

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      process.stdout.write(c.repeat(this.size) + '\n');
+    }
+  }
+}
+
+module.exports = Square;
