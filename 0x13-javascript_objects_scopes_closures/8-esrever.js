6cca65c758ac943226e320ce09f50e41b8eac924
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-10 18:13:20 +0100

    Implement a func that returns a reversed copy of a list without using 'reverse' obviously

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    /* reversedList[reversedList.length] = list[i];
+    the above does the same thing as push().
+    another alternative i found is to use a normal loop
+    with unshift() */
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
