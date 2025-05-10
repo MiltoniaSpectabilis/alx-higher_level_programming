ae078fdd29f637f4513130768c996d0a3c70a892
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-10 15:00:26 +0100

    Implement two instance methods rotate() and double()

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,38 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (
+      Number.isInteger(w) &&
+      Number.isInteger(h) &&
+      w > 0 &&
+      h > 0
+    ) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  rotate() {
+    let temp = null;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+}
+
+module.exports = Rectangle;
