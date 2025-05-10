83d0722b1a3b18abb57de82f652f0cc3cbab7b80
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-10 16:46:57 +0100

    Implement a function that returns the number of occurrences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let occCounter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      occCounter += 1;
+    }
+  }
+  return occCounter;
+};
