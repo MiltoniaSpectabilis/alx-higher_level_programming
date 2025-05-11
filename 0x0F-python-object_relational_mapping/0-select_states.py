d661d305377f328250653a7e1fa0267f17fa4611
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-11 16:56:15 +0100

    Implement a py script that lists all states from a specified database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+username = argv[1]
+password = argv[2]
+database = argv[3]
+
+connection = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database
+)
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM states")
+for row in cursor.fetchall():
+    print(row)
+cursor.close()
+connection.close()
