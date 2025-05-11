03332f3019bbf86cbd75c6e2bc0f63920cd15d42
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-11 20:10:50 +0100

    Implement a py script that prints a specified state from a specified database but the query is now immune to sql injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+
+"""
+
+from MySQLdb import connect
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    connection = connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor = connection.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cursor.execute(query, (state_name,))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    connection.close()
