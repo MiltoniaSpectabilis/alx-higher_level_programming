f5109c3e1c8866e651c71bcfa242d4b334025b60
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-11 19:06:59 +0100

    Use BINARY for case sensitive filtering

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         """
         SELECT *
         FROM states
-        WHERE name = '{}'
+        WHERE BINARY name = '{}'
         ORDER BY states.id ASC
         """.format(state_name)
     )
