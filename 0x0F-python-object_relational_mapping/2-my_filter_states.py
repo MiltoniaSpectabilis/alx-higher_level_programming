7f27268f8bbead59bc4296f929674fa69f666004
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-11 18:59:44 +0100

    Implement a py script that prints a specified state from a specified db

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+This module prints a specified state
+from a specified db
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = connection.cursor()
+    cursor.execute(
+        f"""
+        SELECT *
+        FROM states
+        WHERE name = '{state_name}'
+        ORDER BY states.id ASC
+        """
+    )
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    connection.close()
