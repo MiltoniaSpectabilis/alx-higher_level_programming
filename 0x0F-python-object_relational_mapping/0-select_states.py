be9c1c76852f8ad4f34fcd91aa5e385ce33c75e1
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-11 17:13:04 +0100

    Add main guard and docstring

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+"""This module lists all states from a specified database
+"""
 
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-database = argv[3]
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
-connection = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database
-)
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM states")
-for row in cursor.fetchall():
-    print(row)
-cursor.close()
-connection.close()
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        db=database
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    connection.close()
