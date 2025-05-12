c243357547f1986d2e770372d9556c51f0be6f94
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-12 13:46:33 +0100

    Implement a py script that prints all cities from a specified state but in a different format this time

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+This module prints all the cities from a specified state
+"""
+
+from MySQLdb import connect, cursors
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state = argv[4]
+    connection = connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+    cursor = connection.cursor(cursorclass=cursors.DictCursor)
+    query = """
+    SELECT cities.name
+    FROM states
+    JOIN cities
+    ON state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
+    cursor.execute(query, (state,))
+    cities = [city['name'] for city in cursor]
+    print(", ".join(cities))
