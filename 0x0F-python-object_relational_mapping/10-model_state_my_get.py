3c491765568ddd9606ef30ae6d36b77ff8dee6f2
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-13 12:34:36 +0100

    Implement a py script that prints a specified state from a specified db using sqlalchemy

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+This module prints a specified state from
+a specified db using sqlalchemy
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    state = session.query(State).filter(State.name == state_name).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
