ffbc6a9186a06aff0a0679f5fa22104b9c01d0de
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-13 12:56:57 +0100

    Implement a py script that adds a new state to the db using alchemy ofc

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
+
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+
+    state = session.query(State).filter(State.name == "Louisiana").first()
+    print(state.id)
