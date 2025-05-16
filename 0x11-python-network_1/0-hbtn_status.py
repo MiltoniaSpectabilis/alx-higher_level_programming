fd4aec93a9a8b691d542365c9219f9b8867fdcb8
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-16 14:50:47 +0100

    Remove unnecessary whitespaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urlopen(url) as response:
         body = response.read()
     print("Body response:")
-    print("\t- type: ", type(body))
-    print("\t- content: ", body)
-    print("\t- utf8 content: ", body.decode('utf8'))
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf8'))
