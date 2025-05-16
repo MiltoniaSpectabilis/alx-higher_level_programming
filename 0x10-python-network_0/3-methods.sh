6275a035eec1fdb0b6d93dc3f3e65c83bc30c165
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-05-16 12:54:06 +0100

    Modify script so works specifically with the flask server user for testing

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept
-curl -sIX OPTIONS "$1" | grep methods | cut -f2- -d' '
+curl -sIX OPTIONS "$1" | grep "Allow:" | cut -f2- -d' '
